8dffa8c03f5e2d9006aaa21069a540af7d82b116
sha2waker <51480597+sha2waker@users.noreply.github.com>
2020-04-05 17:43:45 +0430

    Update cam.sh

--- cam.sh
+++ cam.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # SayCheese v1.0
-# coded by: github.com/pulkitsingh256/cam
+# coded by: github.com/sha2waker/cam
 # If you use any part from this code, giving me the credits. Read the Lincense!
 
 trap 'printf "\n";stop' 2
@@ -15,7 +15,7 @@ printf "\e[1;92m       ⁪⁬⁮⁮⁮⁮         ⁪⁬⁮⁮⁮⁮   \e[0m\e"
 printf "\e[1;92m   𝙃𝘼𝘾𝙆 𝙒𝙀𝘽𝘾𝘼𝙈 📸\e[0m\e   \e[0m\n"
 printf "\e[1;92m   Canal telegram: T.me/optax\e[0m                                 \n"
 
-printf " \e[1;77m v1.0 coded by https://github.com/pulkitsingh256/cam\e[0m \n"
+printf " \e[1;77m v1.0 coded by ARTIMSAN\e[0m \n"
 
 printf "\n"
 
